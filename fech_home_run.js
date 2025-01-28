4d302ebda4215d1041bba6f05558349b4bc9eda4
draw-net <takashi@sousato.net>
2025-01-28 20:13:41 -0300

    fech_home_run.js

--- fech_home_run.js
+++ fech_home_run.js
@@ -1,178 +1,154 @@
-  function closeCanvas() {
-    document.getElementById('canvas-container').style.display = 'none';
-  }
-
-  let currentAnimationId;
-
-  function calculateDeformation(speed, distance) {
-    const maxDeformation = 15;
-    const deformationFactor = 0.05;
-    const deformation = speed * distance * deformationFactor;
-    return Math.min(deformation, maxDeformation);
-  }
-
-  function searchHomeRuns() {
-    const playerName = document.getElementById('nome-jogador').value;
-    const resultsDiv = document.getElementById('resultado');
-    resultsDiv.innerHTML = '<p>Carregando...</p>';
-    document.getElementById('video-container').style.display = 'none';
-    document.getElementById('dados-homerun').innerHTML = '';
-
-    const url = playerName ? `http://localhost:3001/get-mlb-home-runs?nome=${encodeURIComponent(playerName)}` : `index.php?nome=${encodeURIComponent(playerName)}`;
-
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        resultsDiv.innerHTML = '';
-        if (data.error) {
-          resultsDiv.innerHTML = `<p style="color:red;">Erro: ${data.error}</p>`;
-        } else if (data.message) {
-          resultsDiv.innerHTML = `<p>${data.message}</p>`;
-        } else if (data && data.length > 0) {
-          const table = document.createElement('table');
-          table.style.borderCollapse = 'collapse';
-          table.style.width = '100%';
-
-          const headerRow = table.insertRow();
-          const headers = ["ID", "Data", "Distância", "Velocidade de Saída", "Ângulo de Saída", "Visualizar", "Vídeo"];
-          headers.forEach(headerText => {
-            const th = document.createElement('th');
-            th.textContent = headerText;
-            th.style.border = '1px solid black';
-            th.style.padding = '8px';
-            headerRow.appendChild(th);
-          });
-
-          data.forEach(hr => {
-            const row = table.insertRow();
-
-            const idCell = row.insertCell();
-            idCell.textContent = hr.id;
-            idCell.style.border = '1px solid black';
-            idCell.style.padding = '8px';
-
-            const dateCell = row.insertCell();
-            dateCell.textContent = hr.game_date;
-            dateCell.style.border = '1px solid black';
-            dateCell.style.padding = '8px';
-
-            const distCell = row.insertCell();
-            distCell.textContent = hr.hit_distance;
-            distCell.style.border = '1px solid black';
-            distCell.style.padding = '8px';
-
-            const velCell = row.insertCell();
-            velCell.textContent = hr.exit_velocity;
-            velCell.style.border = '1px solid black';
-            velCell.style.padding = '8px';
-
-            const angleCell = row.insertCell();
-            angleCell.textContent = hr.launch_angle;
-            angleCell.style.border = '1px solid black';
-            angleCell.style.padding = '8px';
-
-            const viewCell = row.insertCell();
-            const viewButton = document.createElement('button');
-            viewButton.textContent = 'Visualizar';
-            viewButton.onclick = () => visualizeTrajectory(hr.id, hr.hit_distance, hr.exit_velocity, hr.launch_angle, url.includes("http://localhost:3001"));
-            viewCell.appendChild(viewButton);
-            viewCell.style.border = '1px solid black';
-            viewCell.style.padding = '8px';
-
-            const videoCell = row.insertCell();
-            const videoButton = document.createElement('button');
-            videoButton.textContent = 'Video';
-            videoButton.onclick = () => exibirHomeRun(hr, url.includes("http://localhost:3001"));
-            videoCell.appendChild(videoButton);
-            videoCell.style.border = '1px solid black';
-            videoCell.style.padding = '8px';
-          });
-          resultsDiv.appendChild(table);
-        } else {
-          resultsDiv.innerHTML = '<p>Nenhum home run encontrado para este jogador.</p>';
+function closeCanvas(){
+        document.getElementById('canvas-container').style.display = 'none';
+        const canvas = document.getElementById('trajectoryCanvas');
+        if(canvas.dataset.scene){
+            const scene = JSON.parse(canvas.dataset.scene)
+            scene.renderer.dispose();
         }
-      })
-      .catch(error => {
-        resultsDiv.innerHTML = `<p style="color:red;">Erro na requisição: ${error}</p>`;
-      });
-  }
-
-  function visualizeTrajectory(homeRunID, distance, exitVelocity, launchAngle, isNodejs) {
-    document.getElementById('canvas-container').style.display = 'flex';
-    const canvas = document.getElementById('trajectoryCanvas');
-    const ctx = canvas.getContext('2d');
-    const raio = 0.5;
-    let x = 0;
-    let y = canvas.height;
-    let tempo = 0;
-    const deformation = calculateDeformation(parseFloat(exitVelocity), parseFloat(distance));
-    let rotationAngle = 0;
-    const rotationSpeed = 0.1; // Ajuste a velocidade de rotação conforme necessário
-    if (currentAnimationId) {
-      cancelAnimationFrame(currentAnimationId);
+        const iframe = document.getElementById('canvas-iframe');
+        if(iframe){
+           iframe.remove()
+        }
     }
-
-    let initialSpeedX = exitVelocity * Math.cos(launchAngle * (Math.PI / 180));
-    let initialSpeedY = -exitVelocity * Math.sin(launchAngle * (Math.PI / 180));
-
-    function animate() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      x = initialSpeedX * tempo;
-      y = canvas.height - (initialSpeedY * tempo + 0.5 * 9.8 * tempo * tempo);
-      rotationAngle += rotationSpeed; // Atualiza o ângulo de rotação
-
-      ctx.save();
-      ctx.translate(x, y);
-      ctx.rotate(rotationAngle);
-       // Desenhar a bola deformada
-        ctx.beginPath();
-        const deformedRaioY = raio * 10 * (1 + deformation/20);
-         ctx.ellipse(0, 0, raio * 10, deformedRaioY, 0, 0, 2 * Math.PI);
-        ctx.fillStyle = 'red';
-        ctx.fill();
-      ctx.restore();
-      tempo += 0.05;
-
-       if (y > canvas.height || x > canvas.width) {
-        return;
+   function closeVideoOverlay(){
+        document.getElementById('video-overlay').style.display = 'none';
+        const canvas = document.querySelector('#video-overlay-content canvas');
+       if(canvas){
+           cancelAnimationFrame(canvas.animationId)
+            canvas.videoElement.pause();
+           canvas.videoElement = null
+        }
+    }
+    let currentAnimationId;
+  function calculateDeformation(speed, distance){
+        const maxDeformation = 15;
+      const deformationFactor = 0.05;
+        const deformation = speed * distance * deformationFactor;
+       return Math.min(deformation, maxDeformation);
+  }
+   function calculateBallWeight(speed, angle, distance) {
+       const radius = 0.074;
+       const volume = (4/3) * Math.PI * Math.pow(radius, 3);
+        const density = 145;
+       const baseMass = volume * density;
+        const launchAngleInRadians = angle * (Math.PI / 180);
+        const g = 9.81;
+       const crossSectionalArea = 3 * Math.PI * Math.pow(radius, 2);
+      const densityOfAir = 1.225;
+        const dragCoefficient = Math.max(0.01, 1/ (speed * crossSectionalArea ) );
+       const dragForce = 0.5 * dragCoefficient * densityOfAir * crossSectionalArea * Math.pow(speed,2);
+       const effectiveWeight = baseMass + (dragForce * Math.sin(launchAngleInRadians)/g) ;
+        return effectiveWeight;
+  }
+   function calculateRotation(speed, angle) {
+       const radius = 0.074;
+        const exitVelocityMps = speed * 0.44704;
+       const launchAngleRad = angle * Math.PI / 180;
+        const spinEfficiency = 0.7;
+        const spinRateRadPerSec = (exitVelocityMps / radius) * Math.sin(launchAngleRad) * spinEfficiency;
+       const rpm = (spinRateRadPerSec * 60) / (2 * Math.PI);
+      return rpm;
+    }
+    function showVideoOnCanvas(videoUrl, formattedWeight, formattedRPM) {
+        const videoOverlay = document.getElementById('video-overlay');
+      const canvas = document.getElementById('video-canvas');
+        const ctx = canvas.getContext('2d');
+        videoOverlay.style.display = 'flex';
+        const video = document.createElement('video');
+       video.src = videoUrl;
+       video.controls = false;
+        video.onloadedmetadata = function(){
+           canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            video.play();
+            canvas.videoElement = video;
+           drawFrame();
+      }
+        function drawFrame(){
+            if(canvas.videoElement){
+                ctx.drawImage(canvas.videoElement, 0, 0, canvas.width, canvas.height);
+                ctx.font = '18px sans-serif';
+                ctx.fillStyle = 'white';
+                const formattedWeightDisplay = formattedWeight + ' kg';
+                const formattedRPMDisplay =  formattedRPM + ' RPM';
+               ctx.fillText(`Weight: ${formattedWeightDisplay}`, 10, 20);
+               ctx.fillText(`Rotation: ${formattedRPMDisplay}`, 10, 40);
+             canvas.animationId =  requestAnimationFrame(drawFrame);
+          }
        }
-      currentAnimationId = requestAnimationFrame(animate);
-    }
-    animate();
-  }
-
-  function exibirHomeRun(dados, isNodejs) {
-    document.getElementById('video-container').style.display = 'block';
-    document.getElementById('video-source').src = dados.link_video;
-
-    if (isNodejs) {
-      fetch('http://localhost:3001/analyze-video', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ videoUrl: dados.link_video }),
-      })
-        .then(response => response.json())
-        .then(data => {
-          document.getElementById('resultado').textContent = `Volume da bola: ${data.volume.toFixed(4)} metros cúbicos`;
-        })
-        .catch(error => {
-          console.error('Erro ao calcular volume:', error);
-          document.getElementById('resultado').textContent = `Erro ao calcular volume.`;
-        });
-    } else {
-      const raio = 0.074;
-      const volume = 4 / 3 * Math.PI * Math.pow(raio, 3);
-      document.getElementById('resultado').textContent = `Volume da bola (apenas demonstração): ${volume.toFixed(4)} metros cúbicos`;
+       video.onerror = function() {
+           console.error("Erro ao carregar o video:", videoUrl);
+        }
     }
 
-    const dadosHomerunDiv = document.getElementById('dados-homerun');
-    dadosHomerunDiv.innerHTML = `
-       <p>Nome: ${dados.nome}</p>
-        <p>Exit Velocity: ${dados.exit_velocity}</p>
-        <p>Hit Distance: ${dados.hit_distance}</p>
-        <p>Launch Angle: ${dados.launch_angle}</p>
-    `;
-  }
+   function showVideoAndData(homeRunID, data, distance, exitVelocity, launchAngle, videoUrl, formattedWeight, formattedRPM) {
+        const videoOverlay = document.getElementById('video-overlay');
+        const videoContent = document.getElementById('video-overlay-data-container');
+        videoOverlay.style.display = 'flex';
+        videoContent.innerHTML = '';
+       const parts = data.split(" on ");
+        let htmlContent = `
+           <p><strong>${parts[0].trim()}</strong></p>
+        `;
+        videoContent.innerHTML = htmlContent;
+        showVideoOnCanvas(videoUrl, formattedWeight, formattedRPM);
+    }
+    function visualizeTrajectory(homeRunID, distance, exitVelocity, launchAngle, videoUrl) {
+        document.getElementById('canvas-container').style.display = 'flex';
+        const canvas = document.getElementById('trajectoryCanvas');
+        const canvasOverlay = document.getElementById('canvas-overlay-content');
+        const ballWeightLabel = document.getElementById('ballWeightLabel');
+        const iframe = document.getElementById('canvas-iframe');
+        if (iframe){
+            iframe.remove();
+        }
+        const width = canvas.offsetWidth;
+        const height = canvas.offsetHeight;
+        const deformation = calculateDeformation(parseFloat(exitVelocity), parseFloat(distance));
+        const ballWeight = calculateBallWeight(parseFloat(exitVelocity), parseFloat(launchAngle), parseFloat(distance));
+       const rotation = calculateRotation(parseFloat(exitVelocity), parseFloat(launchAngle));
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+        renderer.setSize(width, height);
+        canvas.dataset.scene = JSON.stringify({scene, camera, renderer})
+
+       const geometry = new THREE.SphereGeometry(0.5, 32, 32);
+       const redMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        const deformedBall = new THREE.Mesh(geometry, redMaterial);
+        const deformedScale = 1 + deformation / 40;
+       deformedBall.scale.set(1, deformedScale, 1);
+       const blueMaterial = new THREE.MeshBasicMaterial({
+           color: 0x0000ff,
+           transparent: true,
+           opacity: 0.5,
+           side: THREE.DoubleSide,
+             depthWrite: false
+      });
+        const undeformedBall = new THREE.Mesh(geometry, blueMaterial);
+        undeformedBall.position.z = 0.01;
+        scene.add(deformedBall);
+        scene.add(undeformedBall);
+        camera.position.z = 5;
+       renderer.render(scene, camera);
+        const newIframe = document.createElement('iframe');
+        newIframe.src = videoUrl;
+        newIframe.id = 'canvas-iframe';
+        canvasOverlay.appendChild(newIframe);
+        const formattedWeight = ballWeight.toLocaleString(undefined, { minimumFractionDigits: 4, maximumFractionDigits: 4 });
+      const formattedRotation = rotation.toLocaleString(undefined, { maximumFractionDigits: 0 });
+        ballWeightLabel.innerHTML = `Peso da Bola: ${formattedWeight} kg <br/> Rotação: ${formattedRotation} RPM`;
+    }
+    function searchHomeRuns() {
+        const playerName = document.getElementById('nome-jogador').value;
+        window.location.href = `index.php?nome=${encodeURIComponent(playerName)}`;
+    }
+    document.addEventListener('DOMContentLoaded', function() {
+        const loadingOverlay = document.getElementById('loading-overlay');
+        const nav = document.querySelector('nav');
+        setTimeout(function() {
+            loadingOverlay.style.display = 'none';
+             nav.style.display = 'flex';
+        }, 4200);
+
+    });
